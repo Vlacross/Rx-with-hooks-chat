744dbebed966292f2ccb495e02418248e0974afc
Tim Vlasuk <timvlasuk@gmail.com>
2020-02-17 16:50:13 -0800

    initial app setup

---
+++ README.md
@@ -0,0 +1,4 @@
+coding along in this simple project guide:
+  https://blog.logrocket.com/rxjs-with-react-hooks-for-state-management/
+
+  
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import './App.css';
+
+const App = () => (
+  <Router />
+);
+
+export default App;
