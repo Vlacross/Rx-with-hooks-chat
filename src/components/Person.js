4b3316f0fa9f7d2f763a04097048fc50637f86df
Tim Vlasuk <timvlasuk@gmail.com>
2020-02-20 23:21:36 -0800

    built both person-components

---
+++ src/components/Person.js
@@ -0,0 +1,52 @@
+import React, { useState, useLayoutEffect } from 'react';
+import chatStore from '../store/chat';
+
+const Person = () => {
+  const [chatState, setChatState] = useState(chatStore.initialState);
+
+  console.log(chatState)
+  useLayoutEffect(()=> {
+    chatStore.subscribe(setChatState);
+    chatStore.init();
+  }, []);
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+    const messageObject = {
+      person: "First-Person",
+      text: e.target.elements.messageInput.value.trim()
+    };
+    chatStore.sendMessage(messageObject);
+    document.getElementById('messageForm').reset();
+  };
+
+  return (
+    <div className="container">
+      <h2>
+        Mycrobial
+      </h2>
+      <div className="chat-box">
+        {chatState.data.map((message, index) => (
+          <div key={index}>
+            <p className={message.person}>{message.text}</p>
+            <div className="clear"></div>  
+          </div>
+        ))}
+      </div>
+      <form id="messageForm" onSubmit={onFormSubmit}>
+        <label htmlFor="messageInput">Message Text:</label>
+        <input
+          type="text" 
+          id="messageInput"
+          name="messageInput"
+          placeholder="Enter text here..."
+          required
+           />
+           <button type="submit">Send</button>
+           <button className="clear-button" onClick={() => chatStore.clearChat()}>Clear Chat</button>
+      </form>
+    </div>
+  );
+};
+
+export default Person;
